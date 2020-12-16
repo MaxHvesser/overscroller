c00442ad9aa74c9fe9ca3d14f3b0c6d510b81c48
Maximilian <max.hvesser@finn.no>
2020-12-16 10:53:19 +0100

    Began implementing egde-to-edge

--- app/src/main/java/no/overscroller/sampleapp/basic/BasicListActivity.kt
+++ app/src/main/java/no/overscroller/sampleapp/basic/BasicListActivity.kt
@@ -2,24 +2,42 @@ package no.overscroller.sampleapp.basic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.navigation.NavigationView
 import no.overscroller.sampleapp.R
+import no.overscroller.sampleapp.databinding.ActivityListBasicBinding
 
 class BasicListActivity : AppCompatActivity() {
 
     // region Propertiess
     private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment }
     private val navController by lazy { navHostFragment.navController }
+
+    private lateinit var binding: ActivityListBasicBinding
     // endregion
 
     // region Initialization
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_basic)
+        binding = ActivityListBasicBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupEdgeToEdge()
     }
+    // endregion
+
+    // region View Setup
+    private fun setupEdgeToEdge() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+    // endregion
+
+    // region Back Navigation
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
     // endregion
 
 }
